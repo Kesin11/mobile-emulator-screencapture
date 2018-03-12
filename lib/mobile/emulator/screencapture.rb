7e2c17f5a93a0ec343adab2ad7b9fe787ee09f6a
Kenta Kase <kesin1202000@gmail.com>
2018-03-13 00:19:26 +0900

    Implement create()

--- lib/mobile/emulator/screencapture.rb
+++ lib/mobile/emulator/screencapture.rb
@@ -1,9 +1,21 @@
 require "mobile/emulator/screencapture/version"
+require "mobile/emulator/screencapture/android"
+require "mobile/emulator/screencapture/ios"
 
 module Mobile
   module Emulator
     module Screencapture
-      # Your code goes here...
+      class << self
+        def create(**args)
+          if args[:platform] == "android"
+            Mobile::Emulator::Screencapture::Android.new(args)
+          elsif args[:platform] == "ios"
+            Mobile::Emulator::Screencapture::Ios.new(args)
+          else
+            raise "Not support platform: #{args[:platform]}"
+          end
+        end
+      end
     end
   end
 end
--- test/mobile/emulator/screencapture_test.rb
+++ test/mobile/emulator/screencapture_test.rb
@@ -1,11 +1,15 @@
 require "test_helper"
 
 class Mobile::Emulator::ScreencaptureTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Mobile::Emulator::Screencapture::VERSION
+  def test_create_android
+    android = Mobile::Emulator::Screencapture.create(platform: "android", hoge: "bar")
+
+    assert android
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_create_ios
+    ios = Mobile::Emulator::Screencapture.create(platform: "ios", hoge: "bar")
+
+    assert ios
   end
 end
