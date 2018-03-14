53b2ef59aab9e0d679d120a09572fe51edfa61e3
Kenta Kase <kesin1202000@gmail.com>
2018-03-15 00:20:52 +0900

    Implement screenrecord

--- lib/mobile/emulator/screencapture/android.rb
+++ lib/mobile/emulator/screencapture/android.rb
@@ -19,6 +19,8 @@ module Mobile
         end
 
         def screenshot(image_name)
+          raise "Argument image_name is nil" if image_name.nil?
+
           screenshot_path = File.join(@screenshot_dir, "#{image_name}.png")
           _native_screenshot
           _pull_screenshot(screenshot_path)
@@ -40,9 +42,10 @@ module Mobile
         end
 
         def start_screenrecord(video_name)
+          raise "Argument video_name is nil" if video_name.nil?
           raise "Screenrecord process already started. pid: #{@screenrecord_pid}" if @screenrecord_pid
 
-          @screenrecord_path = File.join(@screenrecord_pid, "#{video_name}.mp4")
+          @screenrecord_path = File.join(@screenrecord_dir, "#{video_name}.mp4")
           @screenrecord_pid = _native_start_screenrecord
 
           @screenrecord_path
@@ -50,7 +53,7 @@ module Mobile
 
           # TODO: add screen record option
         def _native_start_screenrecord
-          pid = spwan("adb shell screenrecord #{DEVICE_SCREENRECORD_PATH}", out: '/dev/null')
+          pid = spawn("adb shell screenrecord #{DEVICE_SCREENRECORD_PATH}", out: '/dev/null')
           Process.detach(pid)
           pid
         end
@@ -72,11 +75,8 @@ module Mobile
           killed_process_num = Process.kill('SIGINT', @screenrecord_pid)
           raise "Kill pid: #{@screenrecord_pid} did not end correctly." unless killed_process_num.positive?
 
-          # For ignore error when process already terminated.
-          begin
-            Process.waitpid(@pid)
-          rescue Errno::ECHILD
-          end
+          # If 'adb pull' after kill recording process soon, the video will be broken.
+          sleep 2
         end
 
         def _pull_screenrecord(screenrecord_path)
